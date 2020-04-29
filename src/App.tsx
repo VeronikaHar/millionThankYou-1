cf0e3a5538119921db4fbd1f05c528411e21409c
Sandhu <jksarpanch@gmail.com>
2020-04-29 15:17:38 -0500

    Added about Us

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/js/bootstrap';
 import './App.css';
 import MainPage from "./components/MainPage/MainPage";
-
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import AboutUs from "./components/AboutUs/AboutUs";
 function App() {
   return (
     <div className="mx-3">
       <header className="App-header">
       </header>
-        <MainPage/>
+        <Router>
+            <div>
+                <Route exact path="/" component={MainPage} />
+                <Route exact path="/aboutUs" component={AboutUs} />
+            </div>
+        </Router>
     </div>
   );
 }
