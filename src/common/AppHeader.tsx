ca6b4c11540b9f0110540fb7447c72c5fa869daf
veronikahar <veronika.har@hotmail.com>
2020-05-01 22:00:09 -0500

    Update navbar.

--- src/common/AppHeader.tsx
+++ src/common/AppHeader.tsx
@@ -5,30 +5,21 @@ class AppHeader extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <a className="navbar-brand logo-image" href="/"><img src={logoImage} alt="Million Thank You" /></a>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/aboutus">About us</a>
-                        </li>
-                    </ul>
-                    <span className="nav-item social-icons ml-auto">
-                        <span className="fa-stack">
-                            <a href="#your-link">
-                                <span className="hexagon"></span>
-                                <i className="fab fa-facebook-f fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span className="fa-stack">
-                            <a href="#your-link">
-                                <span className="hexagon"></span>
-                                <i className="fab fa-twitter fa-stack-1x"></i>
-                            </a>
-                        </span>
-                    </span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/aboutus">About us</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         );
